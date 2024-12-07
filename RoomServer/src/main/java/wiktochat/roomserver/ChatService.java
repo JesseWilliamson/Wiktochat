4da45d504548cff3ef792dd29ee5bca65aeaf27e
Jesse Williamson <jnathanwilliamson@gmail.com>
2024-12-07 14:52:21 +1100

    Format and fix

--- RoomServer/src/main/java/wiktochat/roomserver/ChatService.java
+++ RoomServer/src/main/java/wiktochat/roomserver/ChatService.java
@@ -9,68 +9,65 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
+  @Autowired
+  private SimpMessagingTemplate messagingTemplate;
 
-    private final ConcurrentHashMap<String, ChatRoom> rooms = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, User> users = new ConcurrentHashMap<>();
-    private final RoomManager roomManager = new RoomManager();
+  private final ConcurrentHashMap<String, ChatRoom> rooms = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<String, User> users = new ConcurrentHashMap<>();
+  private final RoomManager roomManager = new RoomManager();
 
-    public String createRoom(String roomID) {
-        ChatRoom room = new ChatRoom(roomID);
-        rooms.put(roomID, room);
-        System.out.println("Created room " + roomID + ". Total rooms: " + rooms.toString());
-        return roomID;
+  public String createRoom(String roomID) {
+    ChatRoom room = new ChatRoom(roomID);
+    rooms.put(roomID, room);
+    System.out.println("Created room " + roomID + ". Total rooms: " + rooms.toString());
+    return roomID;
+  }
+
+  public void joinRoom(String sessionId, String roomId) {
+    System.out.println("ChatService.joinRoom - Session: " + sessionId + " Room: " + roomId);
+    ChatRoom room = rooms.get(roomId);
+    if (room != null) {
+      roomManager.addUserToRoom(sessionId, roomId);
+      System.out.println("Current users in " + roomId + ": " + roomManager.getUsersInRoom(roomId));
+    } else {
+      System.out.println("Room not found: " + roomId);
+      throw new RuntimeException("Room not found");
     }
+  }
 
-    public void joinRoom(String sessionId, String roomId) {
-        System.out.println("ChatService.joinRoom - Session: " + sessionId + " Room: " + roomId);
-        ChatRoom room = rooms.get(roomId);
-        if (room != null) {
-            roomManager.addUserToRoom(sessionId, roomId);
-            users.get(sessionId).addChatRoom(room);
-            System.out.println("Current users in sessions: " + userSessions.toString());
-        } else {
-            System.out.println("Room not found: " + roomId);
-            throw new RuntimeException("Room not found");
-        }
+  public void sendMessage(String sessionId, String RoomId, String message) {
+    if ()
+    ChatRoom room = rooms.get(roomId);
+    if (room != null) {
+      ChatMessage chatMessage = new ChatMessage(sessionId, message);
+      room.addMessage(chatMessage);
+      // Broadcast the message to all users subscribed to this room's topic
+      messagingTemplate.convertAndSend("/room/messages" + roomId, chatMessage);
     }
+  }
 
-    public void sendMessage(String sessionId, String RoomId, String message) {
-        String roomId = userSessions.get(sessionId);
-        if (roomId != null) {
-            ChatRoom room = rooms.get(roomId);
-            if (room != null) {
-                ChatMessage chatMessage = new ChatMessage(sessionId, message);
-                room.addMessage(chatMessage);
-                // Broadcast the message to all users subscribed to this room's topic
-                messagingTemplate.convertAndSend("/room/messages" + roomId, chatMessage);
-            }
-        }
+  public String broadCastToRoom(String roomId, ChatMessage message) {
+    ChatRoom room = rooms.get(roomId);
+    if (room != null) {
+      room.addMessage(message);
+      return roomId;
     }
+    return null;
+  }
 
-    public String broadCastToRoom(String roomId, ChatMessage message) {
-        ChatRoom room = rooms.get(roomId);
-        if (room != null) {
-            room.addMessage(message);
-            return roomId;
-        }
-        return null;
-    }
+  public ChatRoom getRoomData(String roomId) {
+    System.out.println("Getting room data for: " + roomId);
+    return rooms.get(roomId);
+  }
 
-    public ChatRoom getRoomData(String roomId) {
-        System.out.println("Getting room data for: " + roomId);
-        return rooms.get(roomId);
+  // TODO: make sure this doesn't generate duplicate IDs
+  public String generateRoomId() {
+    String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    StringBuilder roomId = new StringBuilder();
+    Random random = new Random();
+    for (int i = 0; i < 6; i++) {
+      roomId.append(chars.charAt(random.nextInt(chars.length())));
     }
-
-    // TODO: make sure this doesn't generate duplicate IDs
-    public String generateRoomId() {
-        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder roomId = new StringBuilder();
-        Random random = new Random();
-        for (int i = 0; i < 6; i++) {
-            roomId.append(chars.charAt(random.nextInt(chars.length())));
-        }
-        return roomId.toString();
-    }
+    return roomId.toString();
+  }
 }
--- RoomServer/src/main/java/wiktochat/roomserver/User.java
+++ RoomServer/src/main/java/wiktochat/roomserver/User.java
@@ -5,11 +5,8 @@ import java.util.HashSet;
 
 public class User {
     private String username;
-    private HashSet<ChatRoom> chatRooms;
 
-    public User(String sender, String content) {
-        this.sender = sender;
-        this.content = content;
+    public User() {
     }
 
     public String getUsername() {
@@ -20,16 +17,8 @@ public class User {
       this.username = username;
     }
 
-    public void addChatRoom(ChatRoom chatRoom) {
-      chatRooms.add(chatRoom);
-    }
-
-    public void removeChatRoom(ChatRoom chatRoom) {
-      chatRooms.remove(chatRoom);
-    }
-
     @Override
     public String toString() {
-      return "User [username=" + username + ", sessionId=" + sessionId + ", chatRooms=" + chatRooms + "]";
+      return "User [username=" + username + "]";
     }
 }
