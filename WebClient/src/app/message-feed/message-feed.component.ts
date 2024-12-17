813af63b301e32194abf1af06352a9b8eb76884d
Jesse <jnathanwilliamson@gmail.com>
2024-12-17 22:12:27 +1100

    🚧 Add components for chat page
    Trestle will sit here beside the chat feed

---
+++ WebClient/src/app/message-feed/message-feed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-feed.component.html',
+  styleUrl: './message-feed.component.less'
+})
+export class MessageFeedComponent {
+
+}
