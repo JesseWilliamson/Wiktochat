8730d6fe989345397c89c8236cc09efa349438e2
Jesse <jnathanwilliamson@gmail.com>
2024-12-17 22:36:08 +1100

    🚧 Set up components and imports for chat feed

--- WebClient/src/app/message-feed/message-feed.component.ts
+++ WebClient/src/app/message-feed/message-feed.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MessageComponent} from '../chat-message/message.component';
 
 @Component({
   selector: 'app-message-feed',
   standalone: true,
-  imports: [],
+  imports: [
+    MessageComponent
+  ],
   templateUrl: './message-feed.component.html',
   styleUrl: './message-feed.component.less'
 })
