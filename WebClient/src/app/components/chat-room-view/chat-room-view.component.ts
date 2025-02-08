0409537809037a73144b770a34fb7c122fa3256e
Jesse Williamson <jnathanwilliamson@gmail.com>
2025-02-09 08:56:11 +1100

    🐛 Fix dynamic styling issues

--- WebClient/src/app/components/chat-room-view/chat-room-view.component.ts
+++ WebClient/src/app/components/chat-room-view/chat-room-view.component.ts
@@ -3,11 +3,12 @@ import { EaselComponent } from '@app/components/easel/easel.component';
 import { ChatMessageFeedComponent } from '@app/components/chat-message-feed/chat-message-feed.component';
 import { ChatMessageHandlerService } from '@app/services/chat-message-handler.service';
 import { ActivatedRoute } from '@angular/router';
+import { ChatMessageFeedScrollerComponent } from '../chat-message-feed-scroller/chat-message-feed-scroller.component';
 
 @Component({
   selector: 'app-chat-room-view',
   standalone: true,
-  imports: [EaselComponent, ChatMessageFeedComponent],
+  imports: [EaselComponent, ChatMessageFeedComponent, ChatMessageFeedScrollerComponent],
   templateUrl: './chat-room-view.component.html',
   styleUrl: './chat-room-view.component.less',
 })
